107872e4a3351f7f54fab9f1cd708b449ab59c90
wangzhen14 <wangzhen14@corp.netease.com>
2023-07-29 22:39:26 +0800

    初始化

---
+++ canal-cleint/src/test/java/com/example/canalcleint/CanalCleintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.canalcleint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanalCleintApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
