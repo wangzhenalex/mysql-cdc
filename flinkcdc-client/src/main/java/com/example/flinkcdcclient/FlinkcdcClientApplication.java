d3532a37c74b2faa30f9a6a16682e35fe706db30
wangzhen14 <wangzhen14@corp.netease.com>
2023-07-30 20:07:06 +0800

    flink cdc

---
+++ flinkcdc-client/src/main/java/com/example/flinkcdcclient/FlinkcdcClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.flinkcdcclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkcdcClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkcdcClientApplication.class, args);
+    }
+
+}
