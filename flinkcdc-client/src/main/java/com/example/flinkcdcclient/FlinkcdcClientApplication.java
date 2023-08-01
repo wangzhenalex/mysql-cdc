2d7fbe35f666641ab57d11b83ee7b7110cd3fc59
wangzhen14 <wangzhen14@corp.netease.com>
2023-08-01 12:46:34 +0800

    cdc

--- flinkcdc-client/src/main/java/com/example/flinkcdcclient/FlinkcdcClientApplication.java
+++ flinkcdc-client/src/main/java/com/example/flinkcdcclient/FlinkcdcClientApplication.java
@@ -1,9 +1,10 @@
 package com.example.flinkcdcclient;
 
+import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
+import com.alibaba.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class FlinkcdcClientApplication {
 
     public static void main(String[] args) {
